f2da2aa6be65fe8d7e6f580107b61c12cfc29532
rafaelvmedeiros <rafaeldesenv.net@gmail.com>
2021-06-25 23:49:45 -0300

    add: user authentication with jwt

---
+++ server/src/app/controllers/SessionController.js
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+
+import authCofing from '../../config/auth';
+import User from '../models/User';
+
+class SessionController {
+  async store(req, res) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      return res.status(401).json({ error: 'User not found' });
+    }
+
+    if (!(await user.checkPassword(password))) {
+      return res.status(401).json({ error: 'Password does not match' });
+    }
+
+    const { id, name } = user;
+
+    return res.json({
+      user: {
+        id,
+        name,
+        email,
+      },
+      token: jwt.sign({ id }, authCofing.secret, {
+        expiresIn: authCofing.expiresIn,
+      }),
+    });
+  }
+}
+
+export default new SessionController();
