4ec232476469313beb75c35acc5183b1511a5dfb
rafaelvmedeiros <rafaeldesenv.net@gmail.com>
2021-06-26 10:36:03 -0300

    fix: id parameters

---
+++ server/src/database/migrations/20210626051803-create-files.js
@@ -0,0 +1,30 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('files', {
+      id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      path: {
+        type: Sequelize.STRING,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('files');
+  },
+};
