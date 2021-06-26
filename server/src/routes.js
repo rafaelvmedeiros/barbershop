bddda7453df75206786965936ab59c6456279980
rafaelvmedeiros <rafaeldesenv.net@gmail.com>
2021-06-26 11:56:04 -0300

    add: file controller to routes

--- server/src/routes.js
+++ server/src/routes.js
@@ -4,6 +4,9 @@ import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
@@ -11,13 +14,15 @@ const routes = new Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
+
 routes.post('/sessions', SessionController.store);
 
 routes.use(authMiddleware);
 
 routes.post('/users', UserController.update);
-routes.post('/files', upload.single('file'), (req, res) => {
-  res.json({ ok: true });
-});
+
+routes.get('/providers', ProviderController.index);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
