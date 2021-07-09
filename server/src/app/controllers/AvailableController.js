80e215355f38757bbbea260a04922c642758ccd7
rafaelvmedeiros <rafaeldesenv.net@gmail.com>
2021-07-09 08:57:52 -0300

    fix: variable references on available controller

--- server/src/app/controllers/AvailableController.js
+++ server/src/app/controllers/AvailableController.js
@@ -47,7 +47,7 @@ class AvailableController {
         value: format(value, "yyyy-MM-dd'T'HH:mm:ssxxx"),
         available:
           isAfter(value, new Date()) &&
-          !Appointment.find((a) => format(a.date, 'HH:mm') === data.time),
+          !appointments.find((a) => format(a.date, 'HH:mm') === data.time),
       };
     });
 
